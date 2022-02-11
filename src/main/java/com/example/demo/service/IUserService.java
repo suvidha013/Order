b8be2593a760709c48b40167ee9439b484f48087
suvidha013 <tcstrainingspringboot@gmail.com>
2022-02-11 16:39:04 +0530

    first commit

---
+++ src/main/java/com/example/demo/service/IUserService.java
@@ -0,0 +1,27 @@
+package com.example.demo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.UserRepository;
+import com.example.demo.entity.User;
+
+@Service
+public class IUserService {
+	@Autowired 
+	UserRepository userRepository;
+	public void saveUser(User user) {
+		userRepository.save(user);
+	}
+
+	public Iterable<User> getUsers() {
+		return userRepository.findAll();
+	}
+
+	public Optional<User> getUser(Integer id) {
+		return userRepository.findById(id);
+	}
+
+}
